b7a2f956c637767cf1b20edbb0d57c3eec89834f
Kathy Reid <kathy@kathyreid.id.au>
2014-07-12 21:11:33 +1000

    Filtered unnamed buildings - ie to encourage City of Melb to name buildings in the data :-)

--- map/overlays.js
+++ map/overlays.js
@@ -29,6 +29,28 @@ function success(position) {
 	var markYouAreHere = L.marker([currentLat, currentLong]).addTo(map);
 	markYouAreHere.bindPopup("<h3>You are here</h3>").openPopup();
 	
+	$.getJSON('http://data.melbourne.vic.gov.au/resource/pmhb-s6pn.json?$where=suburb=%27MELBOURNE%27', function(data) {
+
+        var row;
+        for (row in data) {
+        	
+        	if (data[row].building_name.length < 1) {
+        		var buildingName = data[row].building_name
+        	}
+        	else {
+        		var buildingName = 'Unnamed Building';
+        	}
+        	
+        	 
+        	
+          L.marker(
+          [
+          data[row].latitude,
+          data[row].longitude
+          ]).addTo(map).bindPopup(
+          "<h4>" + buildingName + "</h4>" +
+          "<p>" + data[row].street_address + "</p>").openPopup();
+        }});
 	
 
 
@@ -37,3 +59,25 @@ function success(position) {
 function fail (){
 	alert ("Failed to get geolocation");
 }
+
+
+function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement function
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = (lat2 - lat1) * Math.PI / 180;
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d * 1000; // meters
+}
+
+
+/* 
+
+distanceTo( <LatLng> otherLatlng ) 	Number 	Returns the distance (in meters) to the given LatLng calculated using the Haversine formula. See description on wikipedia
+
+
+*/
+
