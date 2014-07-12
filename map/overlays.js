97b49655ff543e3deb629a332fe8293092450f16
Kathy Reid <kathy@kathyreid.id.au>
2014-07-12 20:09:37 +1000

    Got map working with our look and feel and current user location

--- map/overlays.js
+++ map/overlays.js
@@ -2,4 +2,38 @@
 
 */
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(success, fail);
+	} else {
+	  alert('navigator.geolocation not supported on your browser.');
+	}
+	
+
+
+
+function success(position) {
+	
+	var currentLat = position.coords.latitude;
+	var currentLong = position.coords.longitude;
+
+	var map = L.map('map').setView([currentLat,currentLong], 17);
+	
+	L.tileLayer('http://{s}.tiles.mapbox.com/v3/kathyreid.iohfhi83/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
+
+
+	var youAreHere = L.latLng(currentLat, currentLong);
+	
+	var markYouAreHere = L.marker([currentLat, currentLong]).addTo(map);
+	markYouAreHere.bindPopup("<h3>You are here</h3>").openPopup();
+	
+	
+
+
+} 
+
+function fail (){
+	alert ("Failed to get geolocation");
+}
